1b063e0bb045c6bbfe5389a52c8d21a104a59e4d
georgegausden <george.philip.gausden@gmail.com>
2023-03-30 18:16:19 -0400

    iterativePrototype

--- Assets/AnimationRocketBeginning.cs
+++ Assets/AnimationRocketBeginning.cs
@@ -10,19 +10,27 @@ public class AnimationRocketBeginning : MonoBehaviour
     public float acceleration = 2f;
     private float timer = 5f;
     private bool isMovingForward = true;
+    
 
     private void Update()
     {
+        
+
         if (timer > 0)
         {
             timer -= Time.deltaTime;
             if (isMovingForward)
             {
-                transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                //transform.Translate(Vector3.forward * speed * Time.deltaTime);
             }
         }
         else
         {
+            if (GetComponent<RotateAround>()!= null)
+            {
+                Destroy(GetComponent<RotateAround>());
+            }
+
             isMovingForward = false;
             Vector3 targetDirection = target.transform.position - transform.position;
             targetDirection.y = 0f; // Optional: lock onto the same height
